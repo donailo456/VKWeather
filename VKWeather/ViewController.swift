1658cccaa8d1abbb93b67832d51abbcc3bda93ad
Danil <118375572+donailo456@users.noreply.github.com>
2024-03-19 13:36:40 +0300

    Initial Commit

---
+++ VKWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKWeather
+//
+//  Created by Danil Komarov on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
